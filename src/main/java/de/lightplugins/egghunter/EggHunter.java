d4f8344bba04420726ff4bab9bdc997bc2c5d4e8
lightPlugins <lightningdesign@outlook.de>
2023-12-09 12:37:05 +0100

    Initial commit

---
+++ src/main/java/de/lightplugins/egghunter/EggHunter.java
@@ -0,0 +1,20 @@
+package de.lightplugins.egghunter;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class EggHunter extends JavaPlugin {
+
+    public void onLoad() {
+
+    }
+
+    public void onEnable() {
+
+    }
+
+    public void onDisable() {
+
+    }
+
+
+}
